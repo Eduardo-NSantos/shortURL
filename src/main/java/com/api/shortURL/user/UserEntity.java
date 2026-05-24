5fe89836352ccc1315b2b91ec0f5ea12865d71f5
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-05-23 23:51:30 -0300

    Criação do user

---
+++ src/main/java/com/api/shortURL/user/UserEntity.java
@@ -0,0 +1,43 @@
+package com.api.shortURL.user;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    Integer planId; // temporário
+
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    LocalDateTime updatedAt;
+
+    LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
