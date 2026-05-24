9adedcff0330cd8fb6eb6ab4a222442153a92b71
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-05-24 00:51:42 -0300

    Adicionande fucionalidades úteis ao user

--- src/main/java/com/api/shortURL/user/UserEntity.java
+++ src/main/java/com/api/shortURL/user/UserEntity.java
@@ -1,11 +1,15 @@
 package com.api.shortURL.user;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
