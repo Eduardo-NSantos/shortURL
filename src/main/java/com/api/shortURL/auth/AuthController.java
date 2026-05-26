39df811e82632f9c2fcf941c84fcf9f07a3175d3
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-05-26 11:05:14 -0300

    rotas de autenticação

--- src/main/java/com/api/shortURL/auth/AuthController.java
+++ src/main/java/com/api/shortURL/auth/AuthController.java
@@ -1,4 +1,41 @@
 package com.api.shortURL.auth;
 
+import com.api.shortURL.auth.dto.LoginRequestDTO;
+import com.api.shortURL.auth.dto.LoginResponseDTO;
+import com.api.shortURL.auth.dto.RegisterRequestDTO;
+import com.api.shortURL.auth.dto.RegisterResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @RequestBody
+            @Valid
+            LoginRequestDTO request
+    ){
+        LoginResponseDTO login = service.login(request);
+        return ResponseEntity.ok(login);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(
+            @RequestBody
+            @Valid
+            RegisterRequestDTO request
+    ){
+        RegisterResponseDTO register = service.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(register);
+    }
 }
--- src/main/java/com/api/shortURL/auth/AuthService.java
+++ src/main/java/com/api/shortURL/auth/AuthService.java
@@ -1,4 +1,51 @@
 package com.api.shortURL.auth;
 
+import com.api.shortURL.auth.dto.LoginRequestDTO;
+import com.api.shortURL.auth.dto.LoginResponseDTO;
+import com.api.shortURL.auth.dto.RegisterRequestDTO;
+import com.api.shortURL.auth.dto.RegisterResponseDTO;
+import com.api.shortURL.auth.mapper.RegisterMapper;
+import com.api.shortURL.secutiry.CustomUserDetails;
+import com.api.shortURL.secutiry.TokenService;
+import com.api.shortURL.user.UserService;
+import com.api.shortURL.user.dto.UserRequestDTO;
+import com.api.shortURL.user.dto.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UserService userService;
+    private final RegisterMapper mapper;
+
+    public LoginResponseDTO login(LoginRequestDTO login){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(
+                login.email(),
+                login.password()
+        );
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        if (auth.getPrincipal() == null){
+            throw new RuntimeException("Erro ao fazer login");
+        }
+
+        CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
+        String token = tokenService.generateToken(user);
+
+        return new LoginResponseDTO(token);
+    }
+
+    public RegisterResponseDTO register(RegisterRequestDTO request){
+        userService.assertEmailNotInUse(request.email());
+
+        UserRequestDTO user = mapper.toUserRequestDTO(request);
+        UserResponseDTO saved = userService.save(user);
+
+        return mapper.toRegisterResponseDTO(saved);
+    }
 }
