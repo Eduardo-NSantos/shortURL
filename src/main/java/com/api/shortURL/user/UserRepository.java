3d743967cf1cebc914f6c5dd7ef7e46d2b810081
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-05-25 21:34:33 -0300

    criação de metodos do repository

--- src/main/java/com/api/shortURL/user/UserRepository.java
+++ src/main/java/com/api/shortURL/user/UserRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByEmailAndDeletedAtIsNull(String email);
+    Optional<UserEntity> findByIdAndDeletedAtIsNull(Integer id);
+    boolean existsByEmail(String email);
 }
