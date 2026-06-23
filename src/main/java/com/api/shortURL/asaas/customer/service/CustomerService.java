4883382e3f0186cef6812c50d9162759d0dc667e
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-06-23 17:42:46 -0300

    Serivce de customer

--- src/main/java/com/api/shortURL/asaas/customer/service/CustomerService.java
+++ src/main/java/com/api/shortURL/asaas/customer/service/CustomerService.java
@@ -1,9 +1,10 @@
-package com.api.shortURL.payment.customer.service;
+package com.api.shortURL.asaas.customer.service;
 
-import com.api.shortURL.payment.client.AsaasClient;
-import com.api.shortURL.payment.customer.dto.CreateCustomerRequest;
-import com.api.shortURL.payment.customer.dto.CreateCustomerResponse;
+import com.api.shortURL.asaas.client.AsaasClient;
+import com.api.shortURL.asaas.customer.dto.CreateCustomerRequest;
+import com.api.shortURL.asaas.customer.dto.CreateCustomerResponse;
 import com.api.shortURL.user.UserEntity;
+import com.api.shortURL.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerService {
     private final AsaasClient asaasClient;
+    private final UserRepository userRepository;
 
     public String getOrCreateAsaasCustomer(UserEntity user){
         if (user.getAsaasCustomerId() != null){
@@ -25,6 +27,7 @@ public class CustomerService {
         );
 
         user.setAsaasCustomerId(response.id());
+        userRepository.save(user);
 
         return response.id();
     }
