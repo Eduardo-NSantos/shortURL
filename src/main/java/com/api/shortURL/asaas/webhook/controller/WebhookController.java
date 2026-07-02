70ef954fd2418f18088157f929c34f0590bad242
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-07-01 22:46:10 -0300

    controller do webhook de pagamentos

--- src/main/java/com/api/shortURL/asaas/webhook/controller/WebhookController.java
+++ src/main/java/com/api/shortURL/asaas/webhook/controller/WebhookController.java
@@ -1,4 +1,24 @@
 package com.api.shortURL.asaas.webhook.controller;
 
+import com.api.shortURL.asaas.webhook.dto.WebhookDTO;
+import com.api.shortURL.asaas.webhook.service.WebHookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webhooks")
+@RequiredArgsConstructor
 public class WebhookController {
+    private final WebHookService webHookService;
+
+    @PostMapping("/asaas")
+    public ResponseEntity<Void> receiveWebhook(@RequestBody WebhookDTO payload){
+        webHookService.process(payload);
+
+        return ResponseEntity.ok().build();
+    };
 }
