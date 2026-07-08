553bc09f323880929211121512bdfcdce3fbd629
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-07-08 14:17:37 -0300

    implementação de pagamento em subscriptions

--- src/main/java/com/api/shortURL/subscription/SubscriptionService.java
+++ src/main/java/com/api/shortURL/subscription/SubscriptionService.java
@@ -1,22 +1,31 @@
 package com.api.shortURL.subscription;
 
+import com.api.shortURL.asaas.payment.PaymentEntity;
+import com.api.shortURL.asaas.payment.service.PaymentService;
 import com.api.shortURL.subscription.dto.SubscriptionRequestDTO;
 import com.api.shortURL.subscription.enums.SubscriptionStatus;
 import com.api.shortURL.subscription.mapper.SubscriptionMapper;
+import com.api.shortURL.user.UserEntity;
+import com.api.shortURL.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class SubscriptionService {
     private final SubscriptionRepository repository;
+    private final UserService userService;
+    private final PaymentService paymentService;
     private final SubscriptionMapper mapper;
 
-    public void create(SubscriptionRequestDTO request){
+    public PaymentEntity create(@Valid SubscriptionRequestDTO request){
         if (repository.existsByUserId(request.userId())){
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST,
@@ -24,11 +33,19 @@ public class SubscriptionService {
             );
         }
 
-        SubscriptionEntity subscription = mapper.toEntity(request);
+        UserEntity user = userService.findEntity(request.userId());
+        PaymentEntity payment = paymentService.createPixPayment(
+                user,
+                request.plan().getPrice()
+        );
+
+        SubscriptionEntity subscription = mapper.toEntity(request, payment);
 
         subscription.setStatus(SubscriptionStatus.PENDING);
-        subscription.setExpires_at(LocalDateTime.now().plusMonths(1));
+        subscription.setExpiresAt(LocalDateTime.now().plusMonths(1));
 
         repository.save(subscription);
+
+        return payment;
     }
 }
